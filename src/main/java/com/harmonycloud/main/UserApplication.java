9c849e63b08ce187954bdd03ac629a67a48ba27a
余倩 <qianyu@harmonycloud.cn>
2019-03-11 15:53:47 +0800

    war包

--- src/main/java/com/harmonycloud/main/UserApplication.java
+++ src/main/java/com/harmonycloud/main/UserApplication.java
@@ -3,6 +3,8 @@ package com.harmonycloud.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,7 +14,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan("com.harmonycloud")
 @EnableJpaRepositories("com.harmonycloud.repository")
 @EntityScan("com.harmonycloud.entity")
-public class UserApplication {
+public class UserApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
